be41d3599f30b481266ea46b7d2b97043a2ef73f
Imanda Fachruddin <imanda@Imandas-MacBook-Pro.local>
2020-12-20 17:26:26 +0700

    Add Date

--- app/src/main/java/space/imanda/firstread/MainActivity.kt
+++ app/src/main/java/space/imanda/firstread/MainActivity.kt
@@ -1,8 +1,11 @@
 package space.imanda.firstread
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import space.imanda.firstread.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,5 +32,15 @@ class MainActivity : AppCompatActivity() {
         binding.tvTitle.text = getString(R.string.reader)
         binding.tvSubTitle.text = getString(R.string.reader_are_awesome)
         binding.tvNotesForReader.text = getString(R.string.learn_to_understand)
+        // binding.loInfoDate.tvDate.text = getString(R.string.date)
+
+        setDate()
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun setDate() {
+        val sdf = SimpleDateFormat("dd/MMM/yyyy hh.mm")
+        val currentDate = sdf.format(Date())
+        binding.loInfoDate.tvDate.text = currentDate
     }
 }
