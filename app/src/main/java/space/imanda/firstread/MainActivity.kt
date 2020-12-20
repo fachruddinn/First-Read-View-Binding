e9e407a707450e4d544f5eadad692669b2763012
Imanda Fachruddin <imanda@Imandas-MacBook-Pro.local>
2020-12-20 16:45:59 +0700

    Update To View Binding

--- app/src/main/java/space/imanda/firstread/MainActivity.kt
+++ app/src/main/java/space/imanda/firstread/MainActivity.kt
@@ -1,20 +1,29 @@
 package space.imanda.firstread
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import space.imanda.firstread.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         setupUI()
     }
 
     private fun setupUI() {
-        val title: TextView = findViewById(R.id.tv_title)
+        /*val title: TextView = findViewById(R.id.tv_title)
         val subTitle: TextView = findViewById(R.id.tv_sub_title)
         title.text = "Reader"
-        subTitle.text = "Reader are Awesome"
+        subTitle.text = "Reader are Awesome"*/
+
+        binding.tvTitle.text = getString(R.string.reader)
+        binding.tvSubTitle.text = getString(R.string.reader_are_awesome)
+        binding.tvNotesForReader.text = getString(R.string.learn_to_understand)
     }
 }
