a52eaf6984b583dccc6ef6ca6025ed2b250718f4
Imanda Fachruddin <imanda@Imandas-MacBook-Pro.local>
2020-12-20 16:08:08 +0700

    Set Data Text Programmatically

--- app/src/main/java/space/imanda/firstread/MainActivity.kt
+++ app/src/main/java/space/imanda/firstread/MainActivity.kt
@@ -2,10 +2,19 @@ package space.imanda.firstread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        val title: TextView = findViewById(R.id.tv_title)
+        val subTitle: TextView = findViewById(R.id.tv_sub_title)
+        title.text = "Reader"
+        subTitle.text = "Reader are Awesome"
     }
 }
